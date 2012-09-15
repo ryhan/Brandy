a44adb0380064c2a8ffa5f44cb1567c444035d4c
Tess Rinearson <tess.rinearson@gmail.com>
2012-09-15 19:41:06 -0400

    Turned it into a rails app
    Frontend =>* rails app

--- app/assets/javascripts/ui/searchController.js
+++ app/assets/javascripts/ui/searchController.js
@@ -46,7 +46,17 @@ function updateResults()
 	if (searchModel.query == "")
 	{
 		$('#results')[0].innerHTML = "";
+		$('#helptext').css('opacity', 1);
+		$('#loading').css({opacity: 0, height: 0});
 		return true;
+	}else
+	{
+		$('#helptext').css('opacity', 0);
+		$('#loading').css({opacity: 1, height: '100px'});
+
+		if (searchModel.loadCount >=30){
+			$('#loading').css({opacity: 0, height: 0});
+		}
 	}
 
 	// Make a new request
@@ -103,6 +113,29 @@ function availabilityChecks(text){
 
 	return genSpan('@', twitter) + genSpan('.COM', com) + genSpan('.ME', me);
 }
+
+function loading(){
+	var opts = {
+		lines: 13, // The number of lines to draw
+		length: 7, // The length of each line
+		width: 4, // The line thickness
+		radius: 10, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 0, // The rotation offset
+		color: '#000', // #rgb or #rrggbb
+		speed: 1, // Rounds per second
+		trail: 60, // Afterglow percentage
+		shadow: false, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: 'auto', // Top position relative to parent in px
+		left: 'auto' // Left position relative to parent in px
+	};
+	var target = document.getElementById('loading');
+	var spinner = new Spinner(opts).spin(target);
+}
+loading();
 
 /*
 function searchCtrl($scope)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandy::Application.config.secret_token = 'e87d78f303960496f193a00637447f8acf9440efd40835ef688797922b2be856d730bacd4d5a22cb5325f45166f6c203f89a9ddfaa1a3bff7d015bdfff562c6a'
